91f9fcf58683a180d29f1544d0b44711eab5c209
quanjingchen <quanjingchen@gmail.com>
2023-02-15 20:54:43 -0800

    add primary key

--- schema.sql
+++ schema.sql
@@ -14,7 +14,8 @@ CREATE TABLE reviews (
   reviewer_email varchar(255) not null,
   response varchar(1000) default null,
   helpfulness INTEGER NOT NULL,
-  "date" timestamptz NOT NULL
+  "date" timestamptz NOT NULL,
+  PRIMARY KEY (id)
 );
 
 /* Create other tables and define schemas for them here! */
@@ -22,6 +23,7 @@ CREATE TABLE photos (
   id INTEGER NOT NULL DEFAULT 0,
   review_id INTEGER NOT NULL,
   "url" varchar(1000) not null,
+  PRIMARY KEY (id),
   FOREIGN KEY (review_id)
     REFERENCES reviews (id)
 );
@@ -29,7 +31,8 @@ CREATE TABLE photos (
 CREATE TABLE characteristics (
   id INTEGER NOT NULL DEFAULT 0,
   product_id INTEGER NOT NULL,
-  "name" varchar(100) not null
+  "name" varchar(100) not null,
+  PRIMARY KEY (id)
 );
 
 
@@ -38,6 +41,7 @@ CREATE TABLE characteristic_reviews (
   characteristic_id INTEGER NOT NULL,
   review_id INTEGER NOT NULL,
   "value" SMALLINT CHECK (value BETWEEN 1 AND 5),
+  PRIMARY KEY (id),
   FOREIGN KEY (characteristic_id)
     REFERENCES characteristics (id),
   FOREIGN KEY (review_id)
