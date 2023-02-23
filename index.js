b951614dd98195f1081fe67ea10c348c73cadb89
quanjingchen <quanjingchen@gmail.com>
2023-02-23 09:55:27 -0800

    add verification token for loaderIo

--- index.js
+++ index.js
@@ -11,6 +11,8 @@ app.use(morgan('dev'));
 app.use(express.json());
 app.use(router);
 
+app.use(express.static('public'));
+
 // app.use(express.static(path.join(__dirname, '../client/dist')));
 
 app.listen(PORT, () => {
