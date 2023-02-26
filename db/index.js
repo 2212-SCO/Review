b58e7b88930ee4c5cdd822644b1433011f8fe018
quanjingchen <quanjingchen@gmail.com>
2023-02-25 18:19:49 -0800

    fix typo

--- db/index.js
+++ db/index.js
@@ -1,9 +1,9 @@
-require("dotenv").config();
+// require("dotenv").config();
 
 const { Pool } = require('pg')
 const pool = new Pool({
-  user: process.env.DB_USER || 'qchen',
-  host: process.env.DB_HOST || 'localhost',
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
   port: process.env.DB_PORT
